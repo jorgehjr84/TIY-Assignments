763617209d4ee38df136f05d3825c20b67aa256a
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-05-07 16:01:08 -0400

    First Failed Test

---
+++ string-calculator.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+function toEnglish(word){
+  
+}
+
+expect(toEnglish("zero")).to.equal(0);
+expect(toEnglish("one")).to.equal(1);
+expect(toEnglish("two")).to.equal(2);
