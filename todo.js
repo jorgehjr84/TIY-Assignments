88c8d5c2441aea9788707a99949f8528c54d94f4
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-05-07 13:12:23 -0400

    Updates to todo.js

--- todo.js
+++ todo.js
@@ -1,20 +1,32 @@
-todo.js Updated(For real this time)
+var expect = require('chai').expect;
 
-$> tudo add "Remember the milk";
-added: "Remember the milk"
 
-$> todo ls;//View To do List
-"1) Remember the milk"
+var taskList = [ ];
 
-$> todo done <task#>; //Checks a task off
-  $> todo ls
-"[x] 1) Remember the milk"
+function listTasks(){
+}
 
-$>tudo delete <task#>; //Deletes task
-deleted: <task#>
 
-$> todo move <task#>; // Allows you to move and prioritize
-moved: <task#>
+// I can add a task to my list...
+function addTaskToList(task, list){
+  //What is the task? @parameter task
+  //Where is the task going? @List Parameter
+  //What order / priority? lowest on the bottom(push)
+  return list.push(task);
+}
 
-$> tudo edit <task#>;// Allows you to edit task
-edited: <task#>
+
+
+expect(taskList.length).to.equal(0);
+addTaskToList("Remember the milk", taskList);
+expect(taskList[0]).to.equal("Remember the milk");
+expect(taskList.length).to.equal(1);
+
+
+//expect what?
+addTaskToList("Take out the trash", taskList);
+//expect what now?
+expect(taskList[1]).to.equal("Take out the trash");
+expect(taskList.length).to.equal(2);
+
+console.log("This is a taskList[0]", taskList[1]);
