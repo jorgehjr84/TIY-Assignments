d7231eaa7993af05bd48e05730461d2db50661ef
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-05-08 15:59:00 -0400

    String Calc with Addition(Failed Test)
    I added the addition function to my string - calculator. The test has failed, so I am trying to figure out why.

--- string-calculator.js
+++ string-calculator.js
@@ -1,14 +1,62 @@
 expect = require('chai').expect;
 
 function toNumber(word){
-  if (word === "zero"){
+  if ( word === "zero" ){
     return 0;
+  }
+  if ( word === "one" ){
+    return 1;
+  }
+  if ( word === "two" ){
+    return 2;
+  }
+  if (word === "three" ){
+    return 3;
+  }
+  if ( word === "four" ){
+    return 4;
+  }
+  if ( word === "five" ){
+    return 5;
+  }
+  if (word === "six" ){
+    return 6;
+  }
+  if ( word === "seven"){
+    return 7;
+  }
+  if ( word === "eight"){
+    return 8;
+  }
+  if ( word === "nine"){
+    return 9;
   }
 
 }
 
 
-expect(toNumber).to.exist;
+/*expect(toNumber).to.exist;
 expect(toNumber("zero")).to.equal(0);
 expect(toNumber("one")).to.equal(1);
 expect(toNumber("two")).to.equal(2);
+expect(toNumber("three")).to.equal(3);
+expect(toNumber("four")).to.equal(4);
+expect(toNumber("five")).to.equal(5);
+expect(toNumber("six")).to.equal(6);
+expect(toNumber("seven")).to.equal(7);
+expect(toNumber("eight")).to.equal(8);
+expect(toNumber("nine")).to.equal(9);
+*/
+
+function plus(a,b){
+  if ( toNumber(a) + toNumber(b) === 0 ) {
+  return = 0;
+  }
+}
+
+
+expect(plus).to.exist;
+expect(plus("zero" + "zero").to.equal(0);
+
+
+console.log(plus("zero" + "zero"));
