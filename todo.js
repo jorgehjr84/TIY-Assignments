b5cbd8598699798c60c559f7f456531470ec12ce
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-05-08 09:02:44 -0400

    Sorting in order of task added

--- todo.js
+++ todo.js
@@ -43,10 +43,10 @@ expect(taskList.length).to.equal(5)
 
 
 //How do I delete an item from my taskList???
-taskList.splice(3,1);
+taskList.splice(2,1);
 
 
-taskList.sort();
+taskList.sort([1,2,3,4,5,6]);
 
 
 console.log(taskList[1], taskList[3], taskList[2], taskList[4]);
