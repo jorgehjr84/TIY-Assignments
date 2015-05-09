0613912a17e37b912a6ceae660ddb66dda6573ef
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-05-09 11:34:16 -0400

    Updates to string calc. More lines of addition
    I added more lines of addition. Receiving a failed test.

--- string-calculator.js
+++ string-calculator.js
@@ -55,11 +55,60 @@ function plus(a, b){
   if (toNumber(a) + toNumber(b) === 1){
     return 1;
   }
-  if ( toNumber(a) + toNumeber(b) === 2){
+  if ( toNumber(a) + toNumber(b) === 2){
     return 2;
   }
+  if ( toNumber(a) + toNumber(b) === 3){
+    return 3;
+  }
+  if ( toNumber(a) + toNumber(b) === 4){
+    return 4;
+  }
+  if ( toNumber(a) + toNumber(b) === 5){
+    return 5;
+  }
+  if ( toNumber(a) + toNumber(b) === 6){
+    return 6;
+  }
+  if ( toNumber(a) + toNumber(b) === 7){
+    return 7;
+  }
+  if ( toNumber(a) + toNumber(b) === 8){
+    return 8;
+  }
+  if ( toNumber(a) + toNumber(b) === 9){
+    return 9;
+  }
+  if ( toNumber(a) + toNumber(b) === 10){
+    return 10;
+  }
+  if ( toNumber(a) + toNumber(b) === 11){
+    return 11;
+  }
+  if ( toNumber(a) + toNumber(b) === 12){
+    return 12;
+  }
+  if ( toNumber(a) + toNumber(b) === 13){
+    return 13;
+  }
+  if ( toNumber(a) + toNumber(b) === 14){
+    return 14;
+  }
+  if ( toNumber(a) + toNumber(b) === 15){
+    return 15;
+  }
+  if ( toNumber(a) + toNumber(b) === 16){
+    return 16;
+  }if (toNumber(a) + toNumber(b) === 17){
+    return 17;
+  }
+  if (toNumber(a) + toNumber(b) === 18){
+    return 18;
+  }
+  if (toNumber(a) + toNumber(b) === 19){
+    return 19;
+  }
 }
-
 
 expect(plus).to.exist;
 expect(plus( "zero","zero")).to.equal(0);
@@ -68,6 +117,28 @@ expect(plus( "zero", "two")).to.equal(2);
 expect(plus( "zero", "three")).to.equal(3);
 expect(plus( "zero", "four")).to.equal(4);
 expect(plus( "zero", "five")).to.equal(5);
+expect(plus("zero", "six")).to.equal(6);
+expect(plus("zero", "seven")).to.equal(7);
+expect(plus("zero", "eight")).to.equal(8);
+expect(plus("zero", "nine")).to.equal(9);
+expect(plus("one", "zero")).to.equal(1);
+expect(plus("one", "one")).to.equal(2);
+expect(plus("one", "two")).to.equal(3);
+expect(plus("one", "three")).to.equal(4);
+expect(plus("one", "four")).to.equal(5);
+expect(plus("one", "five")).to.equal(6);
+expect(plus("one", "six")).to.equal(7);
+expect(plus("one", "seven")).to.equal(8);
+expect(plus("one", "eight")).to.equal(9);
+expect(plus("one", "nine")).to.equal(10);
+expect(plus("two", "zero")).to.equal(2);
+expect(plus("two", "one")).to.equal(3);
+expect(plus("two", "two")).to.equal(4);
+expext(plus("two", "three")).to.equal(5);
+expect(plus("two", "four")).to.equal(6);
+expect(plus("two", "five")).to.equal(7);
+expect(plus("two", "six")).to.equal(8);
+expect(plus("two", "eight")).to.equal(9);
 
 
 
