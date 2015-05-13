ca148d4a995450adf7d879d0a2ab3c232c8e08b6
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-05-12 21:13:56 -0400

    Installed some of the links directly in the HTML File. Like still not correct though

---
+++ FullWidthTabs/scripts.js
@@ -0,0 +1,25 @@
+document.head.parentElement.className = "js";
+
+var elements =document.querySelectorAll();
+
+_.forEach(elements, function(element,index, elements){
+	element.addEventListener('click', function(){
+		element.parentElement.clasList.toggle('tab-current')
+	});
+});
+
+alert(is this working);
+
+/*document.head.parentElement.className = "js";
+
+var elements =document.querySelectorAll('h3');
+
+
+
+_.forEach(elements, function(element, index, elements){
+  element.addEventListener('click', function(){
+    console.log(elements, index, element);
+    element.parentElement.classList.toggle('cbp-ntopen');
+});
+});
+*/
